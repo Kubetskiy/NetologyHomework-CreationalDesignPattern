031ad7843df1848eb23abec016015a2e293d7483
Sergey Kubetskiy <kubetskiy@gmail.com>
2022-09-25 16:35:32 +0300

    Homework ready for review.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,8 @@ public class Main {
                 .setAge(21)
                 .setCity("City")
                 .build();
-
-        Person dad = new Person.Builder()
-                .setName("DadName")
-                .setSurName("DadSurName")
-                .setAge(33)
-                .setCity("DadCity")
-                .build();
-
         Person son = mom.newChildBuilder()
                 .setName("Novice")
-                .setSurName("TEST")
                 .build();
         System.out.println();
     }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -11,16 +11,21 @@ public class Person {
         this.city = builder.city;
     }
 
+    public String getName() {return name;}
+
+    public String getSurName() {return surName;}
+
     public boolean hasAge() {return this.age >= 0;}
 
     public void happyBirthday() {this.age++;}
 
-    public boolean hasAddress() {return this.city == null;}
+    public int getAge() {return age;}
+
+    public boolean hasAddress() {return !(this.city == null);}
 
     public String getCity() {return city;}
 
     public void setCity(String city) {this.city = city;}
-
 
     public static class Builder {
         // Необходимые параметры
@@ -30,30 +35,37 @@ public class Person {
         private int age = -1;
         private String city = null;
 
-        public Builder() {
-        }
+        public Builder() {}
 
         public Builder setName(String name) {
+            if (this.name != null) {throw new IllegalStateException("Имя уже есть\n");}
             this.name = name;
             return this;
         }
 
         public Builder setSurName(String surName) {
+            if (this.surName != null) {throw new IllegalStateException("Фамилия уже есть\n");}
             this.surName = surName;
             return this;
         }
 
         public Builder setAge(int age) {
+            if (this.age >= 0) {throw new IllegalStateException("Возраст уже введен\n");}
+            if (age < 0) {throw new IllegalArgumentException("Возраст меньше нуля - рили?\n");}
             this.age = age;
             return this;
         }
 
         public Builder setCity(String city) {
+            if (this.city != null) {throw new IllegalStateException("Город уже задан\n");}
             this.city = city;
             return this;
         }
 
         public Person build() {
+            if (this.name == null || this.surName == null) {
+                throw new IllegalStateException("Имя и Фамилия обязательны\n");
+            }
             return new Person(this);
         }
     }
